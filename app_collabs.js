f0c692c31838e01828ad7605c2111637c23618fd
Yousef Samy <115892143+yousafSamy1@users.noreply.github.com>
2025-11-26 16:25:22 +0200

    Add files via upload

---
+++ app_collabs.js
@@ -0,0 +1,24 @@
+const collabs = [
+  {
+    title: "Mohaymen - Liyam ft Mozartx",
+    thumbnail: "./Images/Mohaymen - Liyam ft Mozartx.png",
+    url: "https://youtu.be/IR66BPIPhJA?si=1kWrI-GIxQ_LmEsr",
+    featured: true
+  },
+
+];
+
+const grid = document.getElementById("collabs-grid");
+
+collabs.forEach(track => {
+  const card = document.createElement("div");
+  card.className = `track-card ${track.featured ? "featured" : ""}`;
+  card.innerHTML = `
+    <img src="${track.thumbnail}">
+    <div class="track-info">
+      <h2>${track.title}</h2>
+    </div>
+  `;
+  card.addEventListener("click", () => window.open(track.url, "_blank"));
+  grid.appendChild(card);
+});
